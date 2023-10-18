ce778548eff7e893c6b70c1b4764e766d4c92d94
IsabellaBoscoC <124316332+IsabellaBoscoC@users.noreply.github.com>
2023-10-17 21:11:51 -0300

    Exercicio05

---
+++ Lista01/Exercicio05/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+class Program
+{
+    static int MDC(int x, int y) //função
+    {
+        if (y == 0) // - caso de parada 
+        {
+            return x; 
+        }
+
+        else
+        {
+            return  MDC(y, x%y);// formula geral 
+        }
+    }
+
+    static void Main() //inicio do programa
+    {
+        int resposta, x, y;
+
+        Console.Write("Por favor, digite um número inteiro para 'x': ");
+        x = int.Parse(Console.ReadLine());
+
+        Console.Write("Por favor, digite um número inteiro para 'y': ");
+        y = int.Parse(Console.ReadLine());
+
+
+        resposta = MDC(x,y);
+        Console.WriteLine(resposta);
+       
+
+        Console.WriteLine("--FIM DO PROGRAMA--");
+    }
+}
+
+
+
+
