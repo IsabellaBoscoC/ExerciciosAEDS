b0f60f8fbfe7bd585dc70e90972384991f7e7d74
IsabellaBoscoC <124316332+IsabellaBoscoC@users.noreply.github.com>
2023-10-17 21:01:00 -0300

    Exercicio02

---
+++ Lista01/Exercicio02/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+class Program
+{
+    static int SomaSerie(int i, int k, int j) //função
+    {
+        if (i >= j)
+        {
+            return j; //caso base
+        }
+
+        else
+        {
+            return i + SomaSerie( i + k,  k,  j);// formula geral
+        }
+    }
+
+
+    static void Main() //inicio do programa
+    {
+        int resposta, i, k, j;
+
+        Console.Write("Por favor, digite um número inteiro para 'i': ");
+        i = int.Parse(Console.ReadLine());
+
+        Console.Write("Por favor, digite um número inteiro para 'j': ");
+        j = int.Parse(Console.ReadLine());
+
+        Console.Write("Por favor, digite um número inteiro para 'k': ");
+        k = int.Parse(Console.ReadLine());
+
+
+        resposta = SomaSerie( i,  k, j);
+        Console.WriteLine(resposta);
+
+
+        Console.WriteLine("--FIM DO PROGRAMA--");
+    }
+}
+
